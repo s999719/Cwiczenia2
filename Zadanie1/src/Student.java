724a3bed4c48f512476536bebb77876f990a7ba0
Jakub Werulik <s999719@pjwstk.edu.pl>
2024-04-07 14:59:06 +0200

    Klasy

---
+++ Zadanie1/src/Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    String fname;
+    String lname;
+    int indexNumber;
+    String email;
+    String adress;
+    double grades[] = {};
+}
---
+++ Zadanie1/src/StudentGroup.java
@@ -0,0 +1,3 @@
+public class StudentGroup {
+    String nazwa;
+}
